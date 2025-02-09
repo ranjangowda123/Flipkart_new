c6ebb170b23fab12582735930bca805e5857afc8
Ranjan <ranjan@reckonsys.com>
2025-02-09 12:30:16 +0530

    changed the placeholder to css

--- pageobjects/home_page.py
+++ pageobjects/home_page.py
@@ -5,13 +5,14 @@ class HomePage:
     def __init__(self,page:Page):
         self.page = page
 
-    search_field = "Search for products, brands and more"
+    search_field = "input[class='desktop-searchBar']"
     search_button = "a.desktop-submit"
     product = ":nth-match(li.product-base, 3)"
 
 
     def enter_into_search_field(self):
-        self.page.get_by_placeholder(self.search_field).fill("men jackets",timeout=400000)
+        self.page.wait_for_selector(self.search_field,state="visible")
+        self.page.locator(self.search_field).fill("men jackets")
 
     def click_on_search_button(self):
         self.page.locator(self.search_button).click()
